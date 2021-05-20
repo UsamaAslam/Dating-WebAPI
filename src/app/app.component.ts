285fe5061356d003a2d40ad28c00e1f0cb694400
Usama Aslam <usama.aslam009@yahoo.com>
2021-05-20 21:17:26 +0800

    Initial desing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'the dating app';
+  users: any;
+
+  constructor(private http: HttpClient){}
+  ngOnInit(){
+    this.getusers();
+  }
+  getusers(){
+    this.http.get('https://localhost:44363/api/Users').subscribe(Response => {
+      this.users= Response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
